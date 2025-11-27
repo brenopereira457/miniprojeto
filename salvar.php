2f8991f6c406d02ab49c64121be1bea23928f1e9
brenopereira457 <brenocurso.p.c@gmail.com>
2025-11-27 10:18:53 -0400

    Add files via upload

--- salvar.php
+++ salvar.php
@@ -17,3 +17,31 @@ if ($produto === '' || $valor_unitario === '' || $quantidade <= 0 || $data_venda
     header('Location: index.php?msg=Preencha+todos+os+campos+corretamente.');
     exit;
 }
+
+$valor_unitario = (flout) $valor_unitario;
+if ($valor_unitario < 0) {
+    header('Location: index.php?msg=Valor+unitário+não+pode+ser+negativo.');
+    exit;
+}
+
+// INSERT com prepared statement
+$sql= "INSERT INTO vendas(produto, valor_unitario, quantidade, data_venda)
+        VALUES (?, ?, ?, ?)"
+
+$stmt= $conn ->prepare($sql);
+if (!$stmt) {
+    header('Location: index.php?msg=Erro+ao+preparar+comando:+'.urlencode($conn ->error));
+    exit;
+}
+
+$stmt->bind_param("sdis", $produto, $valor_unitario, $quantidade, $data_venda);
+
+if ($stmt->execute()) {
+    header('Location: index.php?msg=Venda+registrada+com+sucesso!');
+} else {
+    header('Location: index.php?msg=Erro+ao+registrar+a+venda:+' .urlencode($stmt->error));
+}
+
+$stmt->close();
+$conn->close();
+exit;
